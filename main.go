a62e9ebb11c0e191f6078912b50c4af7a3cee595
PENG <2856149926@qq.com>
2023-10-11 11:23:51 +0800

    引入了redis

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"BookMall/cache"
 	"BookMall/config"
 	"BookMall/route"
 )
 
 func main() {
 	config.Init()
+	cache.Init()
 	r := route.NewRouter()
 	_ = r.Run(config.HttpPort)
 }
